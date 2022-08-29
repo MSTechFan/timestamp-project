fbbcfbc517721b79de3968a94b8a875b2614dea4
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-08-29 12:48:53 -0500

    completing app

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+require('dotenv').config()     
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -19,14 +20,33 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  if(!req.params.date) {
+    const date = new Date()
+    const unixDate = date.getTime()
+    res.json({"unix": unixDate, "utc": date.toUTCString()})
+  }
+  
+  if(!isNaN(req.params.date)){
+    const date = new Date(+(req.params.date));
+    const unixDate = req.params.date
+    res.json({"unix": unixDate,"utc": date.toUTCString()})
+  }
+  const date = new Date(req.params.date)
+  if(date.toUTCString() === "Invalid Date"){
+    res.json({"error": date.toUTCString()})
+  } else {
+        const unixDate = date.getTime()
+        res.json({"unix": unixDate,"utc": date.toUTCString()})
+      }
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
